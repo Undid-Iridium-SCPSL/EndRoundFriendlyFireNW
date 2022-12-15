bdfbef71e5345e5dc6c58eb99b8375c695f5c3ba
undid-iridium <francis.x.irizarry@gmail.com>
2022-12-15 10:09:35 -0500

    Somehow my code got added to gitignore

---
+++ EndRoundFriendlyFire/Config.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using PlayerRoles;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace EndRoundFriendlyFire
+{
+    public class Config
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether debug logs should be shown.
+        /// </summary>
+        [Description("Whether debug logs should be shown.")]
+        public bool Debug { get; set; } = false;
+        
+        /// <summary>
+        /// Gets or sets a value indicating whether override value is provided.
+        /// </summary>
+        [Description("Overrides ConfigFile FriendlyFireMultiplier (or you can do it there).")]
+        public float OverriddenFriendlyFireMultiplier { get; set; } = 0.2f;
+
+        /// <summary>
+        /// Gets of sets a value determining what is sent to the player when FF is enabled
+        /// </summary>
+        [Description("Sets the message players will get when friendly fire is turned on")]
+        public string Message { get; set; } = "<align=center><voffset=28em> <color=#F6511D> Friendly Fire has been enabled! </color></voffset></align>";
+
+        /// <summary>
+        /// Whether the message above will be a broadcast or hint
+        /// </summary>
+        [Description("The message type for notifying players, broadcast / hint")]
+        public MessageType FriendlyFireNotifyingType { get; set; } = MessageType.Hint;
+        
+        /// <summary>
+        /// How long to display FF message
+        /// </summary>
+        [Description("How long to display FF message")]
+        public float MessageDuration { get; set; }  = 0.3f;
+        
+    }
+    public enum MessageType
+    {
+        Broadcast,
+        Hint
+    };
+}
---
+++ EndRoundFriendlyFire/FriendlyFire.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using GameCore;
+using HarmonyLib;
+using Hints;
+using PlayerStatsSystem;
+using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Enums;
+using Log = PluginAPI.Core.Log;
+
+namespace EndRoundFriendlyFire
+{
+    public class FriendlyFire
+    {
+        [PluginConfig]
+        public Config Config;
+
+        [PluginEntryPoint("EndRoundFriendlyFire", "1.0.0", "Friendly fire auto enabled/disable on round ends", "Undid Iridium")]
+        void LoadPlugin()
+        {
+            Instance = this;
+            PluginAPI.Events.EventManager.RegisterEvents(this);
+            Log.Debug("We have started EndRoundFriendlyFire", Config.Debug);
+        }
+
+        public FriendlyFire Instance { get; set; }
+
+        [PluginEvent(ServerEventType.RoundEnd)]
+        void onRoundEnd()
+        {
+            Server.FriendlyFire = true;
+            ConfigFile.ServerConfig.SetString("friendly_fire_multiplier", Instance.Config.OverriddenFriendlyFireMultiplier.ToString());
+            Log.Debug($"Current friendly fire multiplier: { Instance.Config.OverriddenFriendlyFireMultiplier}", Instance.Config.Debug);
+
+            List<Player> allPlayers = Player.GetPlayers();
+            if (Instance.Config.FriendlyFireNotifyingType is MessageType.Hint)
+            {
+                foreach (Player player in allPlayers)
+                {
+                    player.ReceiveHint(Instance.Config.Message, 15);
+                }
+            }
+            else
+            {
+                Server.Init();
+              
+                foreach (Player player in allPlayers)
+                {
+                    player.SendBroadcast(Instance.Config.Message, (ushort)Instance.Config.MessageDuration);
+                }
+                
+                //Server.SendBroadcast(Instance.Config.Message, (ushort)Instance.Config.MessageDuration);
+            }
+            //Reflection because.. NW
+            Traverse.Create<AttackerDamageHandler>().Method("RefreshConfigs").GetValue();
+            Log.Debug($"End FF server with FriendlyFire On", Instance.Config.Debug);
+        }
+        
+        [PluginEvent(ServerEventType.RoundStart)]
+        void onRoundStart()
+        {
+            Server.FriendlyFire = false;
+            Traverse.Create<AttackerDamageHandler>().Method("RefreshConfigs").GetValue();
+            Log.Debug($"Recycled server with FriendlyFire off", Instance.Config.Debug);
+        }
+        
+    }
+}
